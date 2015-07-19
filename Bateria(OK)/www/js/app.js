c41d1d33c638e48cae4cd85d7cf4339b43ccbb41
jesusrodriguez <jesus@MacBook-Pro-de-Jesus.local>
2015-07-19 19:56:16 +0200

    Añadidas nuevas apps

---
+++ Bateria(OK)/www/js/app.js
@@ -0,0 +1,43 @@
+/*function onAppReady() {
+    if( navigator.splashscreen && navigator.splashscreen.hide ) {   // Cordova API detected
+        navigator.splashscreen.hide() ;
+    }
+}
+document.addEventListener("app.Ready", onAppReady, false) ;*/
+
+///--Mi código
+
+var statusdiv;
+
+function init() {
+document.addEventListener("deviceready", onDeviceReady, true);
+statusdiv = document.getElementById("status");
+}
+var onDeviceReady = function() {
+//Listeners para los eventos de la batería
+window.addEventListener("batterycritical", battCrit, false);
+window.addEventListener("batterylow", battLow, false);
+window.addEventListener("batterystatus", battStat, false);
+};
+
+var battCrit = function(info) {
+navigator.notification.alert("El nivel de tu batería es muy bajo");
+drawStatus(info);
+};
+
+var battLow = function(info) {
+navigator.notification.alert("Tu batería se está agotando");
+drawStatus(info);
+};
+
+var battStat = function(info) {
+drawStatus(info);
+};
+
+var drawStatus = function(info){
+var s = "<p><b>Estado de la batería</b><br/>";
+s += "El nivel es del "+info.level + " %" + "<br/>";
+s += "Está enchufado: "+info.isPlugged;
+s += "</p>";
+statusdiv.innerHTML = s;
+};
---
+++ OtraBaseDatos(OK)/README.md
@@ -0,0 +1,18 @@
+Esta aplicación se ha extraído del tutorial de esta página web:
+---------------------------------------------------------------
+http://2noodles.com/miamiforum/viewtopic.php?f=12&t=58
+
+
+
+
+No ha sido necesario el uso de plugins para su funcionamiento.
+
+Ha sido necesario hacer uso de jquery y jquery mobile para poder ponerla en funcionamiento y se ha empleado Ajax.
+
+Como consecuencia de emplear Ajax parece ser que existe un problema con Intel XDK con la versión del CLI de Cordova 4.1.2 por lo que al construir el APK para Android, la aplicación no tiene el funcionamiento esperado. Este problema lo he encontrado reflejado en la web de StackOverflow:
+http://stackoverflow.com/questions/30255454/cordova-hybrid-app-fails-legacy-android-build-works
+
+En esa web se sugiere cambiar la versión del CLI de Cordova por la 3.5 para que Ajax funcione correctamente. Lo he intentado pero esa no ha sido la solución.
+
+He encontrado una manera de solucionarlo. Hasta ahora todas las apps las he empaquetado utilizando la opción Cordova Hybrid App pero si la construyo como una Legacy Hybrid App finalmente la aplicación termina funcionando correctamente.
+
